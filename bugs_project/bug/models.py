6c6a33b74971c2353361aedef97ef96feda6664a
deking254 <dekingsky@gmail.com>
2023-10-05 10:44:11 +0000

    structure a SQLite database and created a django model for the Bug app

--- bugs_project/bug/models.py
+++ bugs_project/bug/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Bug(models.Model):
+    """This defines the bug db model"""
+
+    description = models.CharField(max_length=200)
+    bug_type = models.CharField(max_length=200)
+    report_date = models.DateTimeField("report date")
+    status = models.CharField(max_length=200)
+	
--- bugs_project/bugs_project/settings.py
+++ bugs_project/bugs_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bug.apps.BugConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
